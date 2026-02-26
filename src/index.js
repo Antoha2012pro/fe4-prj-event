90e77792a4183c6a8a93b8ca7313b4876f682d34
Anttoha <167896136+Antoha2012pro@users.noreply.github.com>
2026-02-26 16:10:54 +0100

    make optimization and handlebars

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import debounce from "lodash/debounce";
+import cardTpl from "./templates/card.hbs";
+import skeletonTpl from "./templates/skeleton.hbs";
+import countryTpl from "./templates/country.hbs";
 
 const API_URL = "https://app.ticketmaster.com/discovery/v2";
 const API_KEY = "6C50WXaQQUp17M9iU5gNHG6hsUzxmK7r";
@@ -20,54 +23,95 @@ const els = {
 
 
 const fetchData = async (search, countryCode) => {
+    renderSkeletons();
     try {
-        const response = await fetch(`${API_URL}/events.json?apikey=${API_KEY}&keyword=${search}${countryCode !== "" ? `&countryCode=${countryCode}` : ""}`);
+        const params = new URLSearchParams({
+            apikey: API_KEY,
+            keyword: search
+        });
+
+        if (countryCode) {
+            params.append("countryCode", countryCode);
+        }
+
+        const url = `${API_URL}/events.json?${params}`;
+        const response = await fetch(url);
         if (!response.ok) throw new Error("Помилка");
         const data = await response.json();
 
         renderItems(data);
     } catch (error) {
         console.error(error);
+        renderSkeletons();
+        // els.notRender.cardsItems.innerHTML = "<p>Error loading data</p>";
     }
 }
 
 const renderItems = (data) => {
-    const { forRender, notRender } = els;
-    notRender.cardsItems.innerHTML = "";
-    notRender.heroInpsBoxItems.innerHTML = "";
+    const { notRender } = els;
 
-    // if (ui.weatherLocation) ui.weatherLocation.style.cursor = "help";
-    // if (ui.weatherLocationImg) ui.weatherLocationImg.style.display = "block";
+    // notRender.heroInpsBoxItems.innerHTML = "";
 
-    data._embedded.events.forEach(item => {
-        console.log(item._embedded.venues[0].country.name);
-        console.log(notRender.heroInpsBoxItems);
-        const liItems = document.createElement("li");
-        liItems.classList.add("hero__inps-box-item");
-        liItems.dataset.value = item._embedded.venues[0].country.name;
-        liItems.textContent = liItems.dataset.value;
-        const li = document.createElement("li");
-        li.classList.add("cards__item")
-        const imgUrl =
-            item.images?.find(img => img.ratio === "16_9")?.url ||
-            item.images?.[0]?.url || "";
-        li.innerHTML = `
-                        <img src="${imgUrl}" alt="#" class="cards__item-img">
-                        <h2 class="cards__item-title">${item.name || ""}</h2>
-                        <p class="cards__item-time">${item.dates.start.localDate || ""}</p>
-                        <div class="cards__item-loc">
-                            <svg class="cards__item-loc-img">
-                                <use href="#"></use>
-                            </svg>
-                            <p class="cards__item-loc-text">${item.dates.timezone || ""}</p>
-                        </div>
-                        `;
-        notRender.cardsItems.appendChild(li);
-        notRender.heroInpsBoxItems.appendChild(liItems);
-        console.log(item._embedded.venues[0].country.countryCode);
-        
-    });
+    // const fragment = document.createDocumentFragment();
+
+    // data._embedded.events.forEach(item => {
+    //     //  console.log(item._embedded.venues[0].country.name);
+    //     //  console.log(notRender.heroInpsBoxItems);
+    //     const liItems = document.createElement("li");
+    //     liItems.classList.add("hero__inps-box-item");
+    //     liItems.dataset.value = item._embedded.venues[0].country.name;
+    //     liItems.innerHTML = `<button class="hero__inps-box-item-btn">${liItems.dataset.value}</button>`;
+    //     fragment.appendChild(liItems);
+    //     // console.log(item._embedded.venues[0].country.countryCode);
+    // });
+
+    // notRender.heroInpsBoxItems.appendChild(fragment);
+
+
+    const events = data._embedded.events;
+
+    if (!events.length) {
+        notRender.cardsItems.innerHTML = "<p>No results</p>";
+        return;
+    }
+
+    renderCountries(events);
+
+    const view = events.map(item => ({
+        name: item.name || "",
+        date: item.dates.start.localDate || "",
+        timezone: item.dates.timezone || "",
+        imgUrl:
+            item.images.find(img => img.ratio === "16_9").url ||
+            item.images[0].url || ""
+    }));
+
+    notRender.cardsItems.innerHTML = view.map(cardTpl).join("");
 };
+
+const renderCountries = (events) => {
+    const seen = new Set(); 
+    const view = [];
+
+    for (const e of events) {
+        const country = e._embedded.venues[0].country;
+        if (!country) continue;
+
+        const code = country.countryCode;
+        const name = country.name;
+
+        if (seen.has(code)) continue;
+        seen.add(code);
+
+        view.push({ code, name });
+    }
+
+    els.notRender.heroInpsBoxItems.innerHTML = view.map(countryTpl).join("");
+};
+
+function renderSkeletons(count = 20) {
+    els.notRender.cardsItems.innerHTML = skeletonTpl().repeat(count);
+}
 
 const chooseCountryLocation = (items, btn, isHiddenDef, isHiddenList) => {
     if (isHiddenList === true) {
@@ -95,12 +139,12 @@ const inputsRender = () => {
 }
 
 const runSearch = () => {
-  const search = els.notRender.heroInpsBoxInputSearch.value.trim();
-  const countryCode = els.notRender.heroInpsBoxInputCountry.value.trim();
+    const search = els.notRender.heroInpsBoxInputSearch.value.trim();
+    const countryCode = els.notRender.heroInpsBoxInputCountry.value.trim();
 
-  if (search.length > 2) {
-    fetchData(search, countryCode);
-  }
+    if (search.length > 2) {
+        fetchData(search, countryCode);
+    }
 };
 
 inputsRender();
